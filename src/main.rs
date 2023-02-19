fc739395f97f5fc1516517f644c9b0d81b516ddc
H4estu <zach.wallace@live.com>
2023-02-19 13:05:47 -0800

    filter colored noise waves

--- src/main.rs
+++ src/main.rs
@@ -14,53 +14,66 @@ impl NoiseColor {
         };
 
         return wave;
-    } 
+    }
 }
-
 
 fn main() {
     println!("Hello, world!");
 
     let color = NoiseColor::Brown;
-
     color
         .render()
-        .save_wav16("test/sounds/brown_noise.wav")
+        .filter(3.0, &mut((pass() | lfo(|t| (xerp11(110.0, 880.0, spline_noise(0, t*5.0)), 10.))) >> bandpass()))
+        .save_wav16("test/sounds/brown_noise_filtered.wav")
         .expect("Could not save file.");
 
-    let pink_noise = Wave64::render(44100.0, 3.0, &mut (pink()));
-    let mut pink_filtered = pink_noise.filter(
-        3.0,
-        &mut ((pass() | lfo(|t| (xerp11(110.0, 880.0, spline_noise(0, t * 5.0)), 1.0)))
-            >> bandpass()),
-    );
-    pink_filtered.normalize();
-
-    let brown_noise = Wave64::render(44100.0, 3.0, &mut (brown()));
-    let mut brown_filtered = brown_noise.filter(
-        3.0,
-        &mut ((pass() | lfo(|t| (xerp11(110.0, 880.0, spline_noise(0, t * 5.0)), 1.0)))
-            >> bandpass()),
-    );
-    brown_filtered.normalize();
-
-    let white_noise = Wave64::render(44100.0, 3.0, &mut (white()));
-    let mut white_filtered = white_noise.filter(
-        3.0,
-        &mut ((pass() | lfo(|t| (xerp11(110.0, 880.0, spline_noise(0, t * 5.0)), 1.0)))
-            >> bandpass()),
-    );
-    white_filtered.normalize();
-
-    pink_filtered
+    let color = NoiseColor::Pink;
+    color
+        .render()
+        .filter(3.0, &mut((pass() | lfo(|t| (xerp11(110.0, 880.0, spline_noise(0, t*5.0)), 10.))) >> bandpass()))
         .save_wav16("test/sounds/pink_noise_filtered.wav")
-        .expect("Could not save.");
+        .expect("Could not save file.");
+
+    let color = NoiseColor::White;
+    color
+        .render()
+        .filter(3.0, &mut((pass() | lfo(|t| (xerp11(110.0, 880.0, spline_noise(0, t*5.0)), 10.))) >> bandpass()))
+        .save_wav16("test/sounds/white_noise_filtered.wav")
+        .expect("Could not save file.");    
+
+    // let pink_noise = Wave64::render(44100.0, 3.0, &mut (pink()));
+    // let mut pink_filtered = pink_noise.filter(
+    //     3.0,
+    //     &mut ((pass() | lfo(|t| (xerp11(110.0, 880.0, spline_noise(0, t * 5.0)), 1.0)))
+    //         >> bandpass()),
+    // );
+    // pink_filtered.normalize();
+
+    // let brown_noise = Wave64::render(44100.0, 3.0, &mut (brown()));
+    // let mut brown_filtered = brown_noise.filter(
+    //     3.0,
+    //     &mut ((pass() | lfo(|t| (xerp11(110.0, 880.0, spline_noise(0, t * 5.0)), 1.0)))
+    //         >> bandpass()),
+    // );
+    // brown_filtered.normalize();
+
+    // let white_noise = Wave64::render(44100.0, 3.0, &mut (white()));
+    // let mut white_filtered = white_noise.filter(
+    //     3.0,
+    //     &mut ((pass() | lfo(|t| (xerp11(110.0, 880.0, spline_noise(0, t * 5.0)), 1.0)))
+    //         >> bandpass()),
+    // );
+    // white_filtered.normalize();
+
+    // pink_filtered
+    //     .save_wav16("test/sounds/pink_noise_filtered.wav")
+    //     .expect("Could not save.");
 
     // brown_filtered
     //     .save_wav16("test/sounds/brown_noise_filtered.wav")
     //     .expect("Could not save");
 
-    white_filtered
-        .save_wav16("test/sounds/white_noise_filtered.wav")
-        .expect("Could not save");
+    // white_filtered
+    //     .save_wav16("test/sounds/white_noise_filtered.wav")
+    //     .expect("Could not save");
 }
