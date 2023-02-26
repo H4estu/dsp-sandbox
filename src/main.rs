6a4bd48ae702829214f2492d88457ec385363ad0
H4estu <zach.wallace@live.com>
2023-02-25 16:31:56 -0800

    add normalize flag for render function

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,16 @@ enum NoiseColor {
 
 
 impl NoiseColor {
-    fn render(&self) -> Wave64 {
-        let wave = match self {
+    fn render(&self, normalize: bool) -> Wave64 {
+        let mut wave = match self {
             Self::Brown => Wave64::render(44100.0, 3.0, &mut (brown())),
             Self::Pink => Wave64::render(44100.0, 3.0, &mut (pink())),
             Self::White => Wave64::render(44100.0, 3.0, &mut (white())),
         };
+
+        if normalize {
+            wave.normalize();
+        }
 
         return wave;
     }
@@ -26,23 +30,24 @@ fn main() {
 
     let color = NoiseColor::Brown;
     let mut node = (pass() | lfo(|t| (xerp11(110.0, 880.0, spline_noise(0, t*5.0)), 10.))) >> bandpass();
+    let normalize = true;
 
     color
-        .render()
+        .render(normalize)
         .filter(3.0, &mut node)
         .save_wav16("test/sounds/brown_noise_filtered.wav")
         .expect("Could not save file.");
 
     let color = NoiseColor::Pink;
     color
-        .render()
+        .render(normalize)
         .filter(3.0, &mut node)
         .save_wav16("test/sounds/pink_noise_filtered.wav")
         .expect("Could not save file.");
 
     let color = NoiseColor::White;
     color
-        .render()
+        .render(normalize)
         .filter(3.0, &mut node)
         .save_wav16("test/sounds/white_noise_filtered.wav")
         .expect("Could not save file.");
