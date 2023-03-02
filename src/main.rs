2aa8bb50e8923dae8a99e7b3c7c264dc9ebcaf21
H4estu <zach.wallace@live.com>
2023-03-01 20:25:34 -0800

    get iced window running

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use fundsp::hacker::*;
 use iced::widget::{button, column, text, Column};
+use iced::executor;
+use iced::{Application, Command, Element, Settings, Theme};
 
 
 #[derive(Debug, Clone, Copy)]
@@ -9,31 +11,47 @@ pub enum Message {
 }
 
 
-struct Counter {
-    value: i32,
-}
+// struct Counter {
+    // value: i32,
+// }
+struct Counter;
 
 
-impl Counter {
-    pub fn view(&self) -> Column<Message> {
-        column![
-            button("+").on_press(Message::IncrementPressed),
-            text(self.value).size(50),
-            button("-").on_press(Message::DecrementPressed),
-        ]
+impl Application for Counter {
+    type Executor = executor::Default;
+    type Flags = ();
+    type Message = Message;
+    type Theme = Theme;
+
+    fn new(_flags: ()) -> (Counter, Command<Self::Message>) {
+        (Counter, Command::none())
     }
 
-    pub fn update(&mut self, message: Message) {
-        match message {
-            Message::IncrementPressed => self.value += 1,
-            Message::DecrementPressed => self.value -= 1
-        }
+    fn title(&self) -> String {
+        String::from("🌊")
+    }
+
+    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+        // match message {
+            // Message::IncrementPressed => self.value += 1,
+            // Message::DecrementPressed => self.value -= 1
+        // }
+        Command::none()
+    }
+
+    fn view(&self) -> Element<Self::Message> {
+        // column![
+        //     button("+").on_press(Message::IncrementPressed),
+        //     text(self.value).size(50),
+        //     button("-").on_press(Message::DecrementPressed),
+        // ]
+        "Exploring the waves...".into()
     }
 }
 
 
 fn main() -> iced::Result {
     println!("Exploring the 🌊's...");
-    
-    
+
+    Counter::run(Settings::default())    
 }
