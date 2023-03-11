91eed4a861d3ea1a99e30998191b509a2efbdd46
H4estu <zach.wallace@live.com>
2023-03-11 13:55:30 -0800

    Drawing a line

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,95 @@
 use fundsp::hacker::*;
 use iced::widget::{button, column, horizontal_rule, horizontal_space, row, text};
-use iced::executor;
-use iced::{Alignment, Application, Command, Element, Length, Settings, Theme};
+use iced::widget::canvas;
+use iced::widget::canvas::stroke::{self, Stroke};
+use iced::{Color, executor, Renderer};
+use iced::{Alignment, Application, Command, Element, Length, Point, Rectangle, Settings, Subscription, Theme};
+use iced::{window};
+
+use std::time::Instant;
 
 
 #[derive(Debug, Clone, Copy)]
-pub enum Message {
-    IncrementPressed,
-    DecrementPressed,
+enum Message {
+    // IncreaseFrequency,
+    // IncreaseAmplitude,
+    // DecreaseFrequency,
+    // DecreaseAmplitude,
+    Tick(Instant)
+}
+
+#[derive(Debug)]
+struct Waveform {
+    state: State
+}
+
+#[derive(Debug)]
+struct State {
+    frequency: f32,
+    amplitude: f32,
+    cache: canvas::Cache,
+    now: Instant
+}
+
+impl State {
+    pub fn new() -> State {
+        let (width, height) = window::Settings::default().size;
+        let now = Instant::now();
+
+        State {
+            frequency: 44100.0,
+            amplitude: 1.0,
+            cache: Default::default(),
+            now
+        }
+    }
+
+    pub fn update(&mut self, now: Instant) {
+        self.now = now;
+        self.cache.clear();
+    }
 }
 
 
-struct Counter {
-    value: i32,
+impl<Message> canvas::Program<Message> for State {
+    type State = ();
+
+    fn draw(
+        &self,
+        _state: &Self::State,
+        _theme: &Theme,
+        bounds: Rectangle,
+        _cursor: canvas::Cursor
+    ) -> Vec<canvas::Geometry> {
+
+        let waveform = self.cache.draw(bounds.size(), |frame| {
+            let line = canvas::Path::line(Point::ORIGIN, Point::new(100., 100.));
+            // frame.translate(frame.center() - Point::ORIGIN);
+            frame.fill(&line, Color::BLACK);
+            frame.stroke(&line, Stroke{
+                style: stroke::Style::Solid(Color::from_rgb(255., 0., 0.)),
+                width: 2.0,
+                ..Stroke::default()
+            })
+        });
+
+        vec![waveform]
+    }
 }
 
 
-impl Application for Counter {
+
+impl Application for Waveform {
     type Executor = executor::Default;
     type Flags = ();
     type Message = Message;
     type Theme = Theme;
 
-    fn new(_flags: ()) -> (Counter, Command<Self::Message>) {
+    fn new(_flags: ()) -> (Waveform, Command<Self::Message>) {
         (
-            Counter{value: 0},
+            Waveform {
+                state: State::new()
+            },
             Command::none(),
         )
     }
@@ -38,36 +103,30 @@ impl Application for Counter {
     }
 
     fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
-       match message {
-            Message::IncrementPressed => self.value += 10,
-            Message::DecrementPressed => self.value -= 10
+        match message {
+            // Message::IncreaseFrequency => self.state.frequency += 10.0,
+            // Message::DecreaseFrequency => self.state.frequency -= 10.0,
+            // Message::IncreaseAmplitude => self.state.amplitude += 1.0,
+            // Message::DecreaseAmplitude => self.state.amplitude -= 1.0
+            Message::Tick(instant) => self.state.update(instant),
         };
         Command::none()
     }
 
     fn view(&self) -> Element<Self::Message> {
-        let button_col = column![
-            text("Incremening Button"),
-            horizontal_rule(10),
-            button("+").on_press(Message::IncrementPressed),
-            text(self.value).size(50),
-            button("-").on_press(Message::DecrementPressed),
-        ]
-        .padding(50)
-        .align_items(Alignment::Center);
+        canvas::Canvas::new(&self.state)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .into()
+    }
 
-        button_col
-        // .push(horizontal_space(Length::Fill))
-        .push(horizontal_rule(10))
-        .push(button("hello"))
-        .into()
-
+    fn subscription(&self) -> Subscription<Message> {
+        window::frames().map(Message::Tick)
     }
 }
-
 
 fn main() -> iced::Result {
     println!("Exploring the 🌊's...");
 
-    Counter::run(Settings::default())    
+    Waveform::run(Settings::default())    
 }
