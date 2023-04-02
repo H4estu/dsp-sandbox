a97da51486b74d27e6eccd5c9d91fc8d8b810659
H4estu <zach.wallace@live.com>
2023-04-01 19:18:49 -0700

    Play chained sine waves on button press

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ use anyhow;
 
 use cpal::{
     traits::{DeviceTrait, HostTrait, StreamTrait},
-    FromSample, Sample, SizedSample,
 };
 
-use fundsp::hacker::*;
+use dasp::{signal, Sample, Signal};
+
+// use fundsp::hacker::*;
 
 use iced::widget::{button, column, horizontal_rule, horizontal_space, row, text};
 use iced::widget::canvas;
@@ -15,7 +16,8 @@ use iced::{Alignment, Application, Command, Element, Length, Point, Rectangle, S
 use iced::{window};
 
 use std::process::Command as ShellCommand;
-use std::time::Instant;
+use std::sync::mpsc;
+use std::time::{Duration, Instant};
 
 
 #[derive(Debug, Clone, Copy)]
@@ -132,7 +134,8 @@ impl Application for Waveform {
                 self.is_playing = !self.is_playing;
 
                 if self.is_playing {
-                    play_sound()
+                    // play_sound()
+                    play_sound_cpal()
                 }
             }
         };
@@ -156,6 +159,17 @@ impl Application for Waveform {
     // fn subscription(&self) -> Subscription<Message> {
     //     window::frames().map(Message::Tick)
     // }
+}
+
+fn play_sound_cpal() {
+    let (_host, device, config) = host_device_setup().unwrap();
+
+    // Only care about f32 for now.
+    match config.sample_format() {
+        cpal::SampleFormat::F32 => run::<f32>(&device, &config.into()),
+        _ => panic!("Not implemented for non-f32"),
+    }.unwrap();
+
 }
 
 fn play_sound() {
@@ -190,10 +204,70 @@ fn host_device_setup() -> Result<(cpal::Host, cpal::Device, cpal::SupportedStrea
     Ok((host, device, config))
 }
 
+fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> Result<(), anyhow::Error> 
+where 
+    T: cpal::Sample + cpal::SizedSample + cpal::FromSample<f32>,
+{
+    // Signal chain to play back one second of each oscillator at A4
+    let hz = signal::rate(config.sample_rate.0 as f64).const_hz(440.);
+    let one_sec = config.sample_rate.0 as usize;
+    let mut synth = hz
+        .clone()
+        .sine()
+        .take(one_sec)
+        .chain(hz.clone().saw().take(one_sec))
+        .chain(hz.clone().square().take(one_sec))
+        .chain(hz.clone().noise_simplex().take(one_sec))
+        .chain(signal::noise(0).take(one_sec))
+        .map(|s| s.to_sample::<f32>() *0.2);
+
+    // A channel for indicating when playback has completed
+    let (complete_tx, complete_rx) = mpsc::sync_channel(1);
+
+    // Create and run the stream
+    let err_fn = |err| eprintln!("An error occured on the stream: {}", err);
+    let channels = config.channels as usize;
+    let stream = device.build_output_stream(
+        config,
+        move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
+            write_data(data, channels, &complete_tx, &mut synth)
+        },
+        err_fn,
+        Some(Duration::new(5, 0))
+    )?;
+
+    stream.play()?;
+
+    // wait for playback to complete. Sounds won't play without these.
+    complete_rx.recv().unwrap();
+    stream.pause()?;
+
+    Ok(())
+
+}
+
+
+fn write_data<T>(output: &mut [T], channels: usize, complete_tx: &mpsc::SyncSender<()>, signal: &mut dyn Iterator<Item = f32>) 
+where
+    T: cpal::Sample + cpal::FromSample<f32>,
+{
+    for frame in output.chunks_mut(channels) {
+        let sample = match signal.next() {
+            None =>  {
+                complete_tx.try_send(()).ok();
+                0.0
+            },
+            Some(sample) => sample,
+        };
+        let value: T = cpal::Sample::from_sample::<f32>(sample);
+        for sample in frame.iter_mut() {
+            *sample = value;
+        }
+    }
+}
+
 fn main() -> iced::Result {
     println!("Exploring the 🌊's...");
-
-    let (_host, _device, _config) = host_device_setup().unwrap();
 
     Waveform::run(Settings::default())    
 }
