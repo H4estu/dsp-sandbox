96eca941c4309c45cf147599685b4bc0c5cf7d62
H4estu <zach.wallace@live.com>
2023-04-04 21:09:44 -0700

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,16 @@ use dasp::{signal, Sample, Signal};
 
 // use fundsp::hacker::*;
 
-use iced::widget::{button, column, horizontal_rule, horizontal_space, row, text};
-use iced::widget::canvas;
-// use iced::widget::canvas::stroke::{self, Stroke};
-use iced::{Color, executor, Renderer};
-use iced::{Alignment, Application, Command, Element, Length, Point, Rectangle, Settings, Subscription, Theme};
-use iced::{window};
+use iced::widget::{button, column, horizontal_rule, text};
+use iced::{Alignment, Application, executor, Command, Element, Settings, Theme};
 
 use std::process::Command as ShellCommand;
 use std::sync::mpsc;
-use std::time::{Duration, Instant};
+use std::time::{Duration};
 
 
 #[derive(Debug, Clone, Copy)]
 enum Message {
-    // IncreaseFrequency,
-    // IncreaseAmplitude,
-    // DecreaseFrequency,
-    // DecreaseAmplitude,
-    // Tick(Instant)
     TogglePlayback,
 }
 
@@ -34,67 +25,6 @@ enum Message {
 struct Waveform {
     is_playing: bool,
 }
-
-// #[derive(Debug)]
-// struct Waveform {
-//     state: State
-// }
-
-// #[derive(Debug)]
-// struct State {
-//     frequency: f32,
-//     amplitude: f32,
-//     cache: canvas::Cache,
-//     now: Instant
-// }
-
-// impl State {
-//     pub fn new() -> State {
-//         let (width, height) = window::Settings::default().size;
-//         let now = Instant::now();
-
-//         State {
-//             frequency: 44100.0,
-//             amplitude: 1.0,
-//             cache: Default::default(),
-//             now
-//         }
-//     }
-
-//     pub fn update(&mut self, now: Instant) {
-//         self.now = now;
-//         self.cache.clear();
-//     }
-// }
-
-
-// impl<Message> canvas::Program<Message> for State {
-//     type State = ();
-
-//     fn draw(
-//         &self,
-//         _state: &Self::State,
-//         _theme: &Theme,
-//         bounds: Rectangle,
-//         _cursor: canvas::Cursor
-//     ) -> Vec<canvas::Geometry> {
-
-//         let waveform = self.cache.draw(bounds.size(), |frame| {
-
-//             let line = canvas::Path::line(Point::ORIGIN, Point::new(100., 100.));
-//             frame.translate(frame.center() - Point::ORIGIN);
-//             frame.fill(&line, Color::BLACK);
-//             frame.stroke(&line, Stroke{
-//                 style: stroke::Style::Solid(Color::from_rgb(255., 0., 0.)),
-//                 width: 2.0,
-//                 ..Stroke::default()
-//             })
-//         });
-
-//         vec![waveform]
-//     }
-// }
-
 
 
 impl Application for Waveform {
@@ -105,9 +35,6 @@ impl Application for Waveform {
 
     fn new(_flags: ()) -> (Waveform, Command<Self::Message>) {
         (
-            // Waveform {
-            //     state: State::new()
-            // },
             Waveform {
                 is_playing: false,
             },
@@ -125,11 +52,6 @@ impl Application for Waveform {
 
     fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
         match message {
-            // Message::IncreaseFrequency => self.state.frequency += 10.0,
-            // Message::DecreaseFrequency => self.state.frequency -= 10.0,
-            // Message::IncreaseAmplitude => self.state.amplitude += 1.0,
-            // Message::DecreaseAmplitude => self.state.amplitude -= 1.0
-            // Message::Tick(instant) => self.state.update(instant),
             Message::TogglePlayback => {
                 self.is_playing = !self.is_playing;
 
@@ -143,10 +65,6 @@ impl Application for Waveform {
     }
 
     fn view(&self) -> Element<Self::Message> {
-        // canvas::Canvas::new(&self.state)
-            // .width(Length::Fill)
-            // .height(Length::Fill)
-            // .into()
         column![
             text("Toggle Play"),
             horizontal_rule(10),
@@ -155,10 +73,6 @@ impl Application for Waveform {
         .align_items(Alignment::Center)
         .into()
     }
-
-    // fn subscription(&self) -> Subscription<Message> {
-    //     window::frames().map(Message::Tick)
-    // }
 }
 
 fn play_sound_cpal() {
