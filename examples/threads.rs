94941f2fa5960e8c7151cd57bd686ae70fd431d6
H4estu <zach.wallace@live.com>
2023-04-17 22:04:58 -0700

    thread munging with channels

--- examples/threads.rs
+++ examples/threads.rs
@@ -1,4 +1,4 @@
-use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
@@ -12,14 +12,30 @@ fn spawn_thread(duration: u64) {
     });
 }
 
+fn channel_test(duration: u64) {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        for i in 1..10 {
+            let to_send = i;
+            tx.send(to_send).unwrap();
+            let received = rx.recv().unwrap();
+            println!("Received value: {}", received);
+            thread::sleep(Duration::from_millis(duration));
+        }
+    });
+}
+
 fn main() {
     println!("Testing thread behavior...");
 
     // time to sleep for, in milliseconds
     let sleep_spawned = 1;
     let sleep_main = 50;
+    let sleep_channel = 10;
     
     spawn_thread(sleep_spawned);
+    channel_test(sleep_channel);
 
     for i in 1..5 {
         println!("hi number {} from main thread", i);
