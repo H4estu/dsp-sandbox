d4f755083d3f97fa340a27eb9b2d0d197ab92ab9
H4estu <zach.wallace@live.com>
2023-04-26 20:30:58 -0700

    Add option for playing signal

--- examples/rodio.rs
+++ examples/rodio.rs
@@ -10,46 +10,40 @@ use termion::event::{Event, Key, MouseEvent};
 use termion::raw::IntoRawMode;
 
 
-fn main() {
+fn play() {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
     
     let source = SineWave::new(440.0).amplify(0.10);
-    // sink.append(source);
+    sink.append(source);
 
     // sink.sleep_until_end();
 
-    // for _ in 0..5 {
-    //     thread::sleep(Duration::from_secs(1));  // Start sound on initial loop
-    //     sink.pause();  // pause after 1 second.
+    for _ in 0..1 {
+        thread::sleep(Duration::from_secs(1));  // Start sound on initial loop
+        sink.pause();  // pause after 1 second.
 
-    //     thread::sleep(Duration::from_secs(1));
-    //     sink.play();
-    // }
+        thread::sleep(Duration::from_secs(1));
+        sink.play();
+    }
+}
 
-    // let mut buffer = String::new();
-    // let input = match io::stdin().read_line(&mut buffer) {
-    //     Ok(n) => {
-    //         println!("Bytes read: {}", n);
-    //         println!("User entered: {}", buffer);
-    //     },
-    //     Err(e) => {
-    //         println!("Cannae read: {e}.")
-    //     }
-    // };
-
+fn main() {
     let stdin = stdin();
     let mut stdout = stdout().into_raw_mode().unwrap();
     // let mut stdout = MouseTerminal::from(stdout().into_raw_mode().unwrap());
     // write!(stdout, "{}{}q to exit. Click, click, click!", termion::clear::All, termion::cursor::Goto(1, 1)).unwrap();
     stdout.flush().unwrap();
-    println!("Captures Mouse Key events\r\nPress q to quit.\r");
+    println!("Captures Mouse Key events.");
+    println!("\rPress p to play sound.");
+    println!("\rPress q to quit.");
 
     for c in stdin.events() {
 
         let event = c.unwrap();
         match event {
             Event::Key(Key::Char('q')) => break,
+            Event::Key(Key::Char('p')) => play(),
             // Event::Key(Key::Char(c)) => c,
             Event::Mouse(_) => todo!("Mouse events"),
             Event::Key(Key::Left) => todo!("Left arrow key"),
