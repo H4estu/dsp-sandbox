9217775d738d783f3a47401599d3e8726834c4de
H4estu <zach.wallace@live.com>
2023-05-29 16:29:27 -0700

    Create infinite sine signal

--- examples/synth.rs
+++ examples/synth.rs
@@ -32,16 +32,17 @@ where
 {
     // Signal chain to play back one second of each oscillator at A4
     let hz = signal::rate(config.sample_rate.0 as f64).const_hz(440.);
-    let one_sec = (config.sample_rate.0) as usize;
-    let mut synth = hz
-        .clone()
-        .sine()
-        .take(one_sec)
-        .chain(hz.clone().saw().take(one_sec))
-        .chain(hz.clone().square().take(one_sec))
-        .chain(hz.clone().noise_simplex().take(one_sec))
-        .chain(signal::noise(0).take(one_sec))
-        .map(|s| s.to_sample::<f32>() *0.2);
+    // let one_sec = (config.sample_rate.0) as usize;
+    // let mut synth = hz
+    //     .clone()
+    //     .sine()
+    //     .take(one_sec)
+    //     .chain(hz.clone().saw().take(one_sec))
+    //     .chain(hz.clone().square().take(one_sec))
+    //     .chain(hz.clone().noise_simplex().take(one_sec))
+    //     .chain(signal::noise(0).take(one_sec))
+    //     .map(|s| s.to_sample::<f32>() *0.2);
+    let mut synth = hz.sine();
 
     // A channel for indicating when playback has completed
     let (complete_tx, complete_rx) = mpsc::sync_channel(1);
@@ -69,18 +70,20 @@ where
 }
 
 
-fn write_data<T>(output: &mut [T], channels: usize, complete_tx: &mpsc::SyncSender<()>, signal: &mut dyn Iterator<Item = f32>) 
+// fn write_data<T>(output: &mut [T], channels: usize, complete_tx: &mpsc::SyncSender<()>, signal: &mut dyn Iterator<Item = f32>) 
+fn write_data<T>(output: &mut [T], channels: usize, complete_tx: &mpsc::SyncSender<()>, signal: &mut signal::Sine<signal::ConstHz>) 
 where
     T: cpal::Sample + cpal::FromSample<f32>,
 {
     for frame in output.chunks_mut(channels) {
-        let sample = match signal.next() {
-            None =>  {
-                complete_tx.try_send(()).ok();
-                0.0
-            },
-            Some(sample) => sample,
-        };
+        // let sample = match signal.next() {
+        //     None =>  {
+        //         complete_tx.try_send(()).ok();
+        //         0.0
+        //     },
+        //     Some(sample) => sample,
+        // };
+        let sample = signal.next() as f32;
         let value: T = cpal::Sample::from_sample::<f32>(sample);
         for sample in frame.iter_mut() {
             *sample = value;
@@ -102,4 +105,9 @@ fn main() {
 
     let (_host, device, ssg) = host_device_setup().unwrap();
     play_signal(device, ssg);
+
+    // let mut sine_signal = signal::rate(44_100.0).const_hz(400.0).sine();
+    // while sine_signal.next() != 2.0 {
+    //     println!("Next frame: {}", sine_signal.next())
+    // }
 }
